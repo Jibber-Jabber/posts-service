4bfd2a674911da5e8c879a168dc90da496cb7b4e
Braian Aniel Biale <braian.biale@ing.austral.edu.ar>
2021-06-17 17:46:17 -0300

    test deploy automation

--- src/main/java/edu/austral/ingsis/jj/postsservice/PostsServiceApplication.java
+++ src/main/java/edu/austral/ingsis/jj/postsservice/PostsServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PostsServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Holaaa");
 		SpringApplication.run(PostsServiceApplication.class, args);
 	}
 
