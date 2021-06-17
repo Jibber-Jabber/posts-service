428d59db0d914b634e30e2abfe063fb89e0d5a6e
Braian Aniel Biale <braian.biale@ing.austral.edu.ar>
2021-06-17 19:29:55 -0300

    test deploy automation, added env variables

--- src/main/java/edu/austral/ingsis/jj/postsservice/PostsServiceApplication.java
+++ src/main/java/edu/austral/ingsis/jj/postsservice/PostsServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PostsServiceApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Otro cambioo");
+		System.out.println("Otro ddd");
 		SpringApplication.run(PostsServiceApplication.class, args);
 	}
 
