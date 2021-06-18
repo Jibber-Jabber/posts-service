8a845bf7640551a492959822a900cf7e677eb417
AgustindelaVega <agustin.delavega@ing.austral.edu.ar>
2021-06-18 02:38:17 -0300

    worflow fix

--- src/main/java/edu/austral/ingsis/jj/postsservice/PostsServiceApplication.java
+++ src/main/java/edu/austral/ingsis/jj/postsservice/PostsServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PostsServiceApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Otro 33333");
 		SpringApplication.run(PostsServiceApplication.class, args);
 	}
 
